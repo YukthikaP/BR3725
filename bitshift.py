31dea96b9058251fb5e552262e7bf01672a1cbc7
“Yukthika <yukthikapalavelli@gmail.com>
2025-06-17 23:27:28 -0700

    multiply by 2 implemented

--- bitshift.py
+++ bitshift.py
@@ -1,5 +1,4 @@
 def multiply(n):
-    #Add the code and an explanation on how to 
-    #multiply a number by 2 using bitshift
-    return n
-
+    return n << 1
+if __name__ == "__main__":
+    print(multiply(9))
